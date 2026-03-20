b7c3c74ee2456ed08cc34a1578b923eb0b4fa929
Nicolas O <nicolasolaya27@gmail.com>
2026-03-20 15:56:33 -0500

    First commit

---
+++ src/main/java/com/ToJrsBack/ToJrsBackApplication.java
@@ -0,0 +1,13 @@
+package com.ToJrsBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToJrsBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToJrsBackApplication.class, args);
+	}
+
+}
